c4b3ebe93d7988a3776a55e31c6aec51583e84f1
Ellis Burgess <ellismburges@gmail.com>
2024-04-03 15:13:03 +0100

    Display page for each car

--- showroom/routes.py
+++ showroom/routes.py
@@ -4,4 +4,10 @@ from showroom.models import Car
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
-    return render_template('index.html', title='Car Showroom')
+    cars = Car.query.all()
+    return render_template('index.html', title='Car Showroom', cars=cars)
+
+@app.route("/car/<int:car_id>")
+def car_page(car_id):
+    car = Car.query.get_or_404(car_id)
+    return render_template('car.html', title=car.name, car=car)
